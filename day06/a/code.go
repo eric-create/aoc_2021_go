642c7625b663d16c9c659c64623b282b26399f09
eric-create <eric.merath@mhp.com>
2024-07-08 18:10:17 +0200

    Day 6: Finished task A.

---
+++ day06/a/code.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"eric-create/aoc_2021/utils"
+	"fmt"
+)
+
+func main() {
+	lines, _ := utils.ReadLines("input.txt")
+	numbers := utils.ExtractInts((*lines)[0])
+	pools := Populate(numbers, [10][]int{})
+	pools = CountDown(80, pools)
+	fmt.Println(FishSum(pools))
+}
+
+func CountDown(days int, pools [10][]int) [10][]int {
+	for day := 0; day < days; day++ {
+
+		// Shift zeroes to temporary pool (index 10)
+		pools[9] = pools[0]
+
+		// Shift everybody else to lower time pool
+		for i := 0; i < 8; i++ {
+			pools[i] = pools[i+1]
+		}
+
+		pools[6] = append(pools[6], pools[9]...)
+		pools[8] = pools[9]
+		pools[9] = []int{}
+	}
+
+	return pools
+}
+
+func FishSum(pools [10][]int) int {
+	sum := 0
+
+	for _, pool := range pools {
+		sum += len(pool)
+	}
+
+	return sum
+}
+
+func Populate(numbers []int, pools [10][]int) [10][]int {
+	for _, n := range numbers {
+		pools[n] = append(pools[n], n)
+	}
+
+	return pools
+}
+
+// func Pools() [9][]int {
+// 	pools := [9][]int{}
+// 	return pools
+// }
